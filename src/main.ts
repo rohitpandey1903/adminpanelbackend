86d59fa86d56c375be8fa38e89199d0ca8a797b2
Rohit Pandey <rd8614196@gmail.com>
2025-03-18 11:11:58 +0530

    Added Cors Config

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableCors({
+    "origin": "*", //allowed everything
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204
+  });
+  await app.listen(process.env.PORT ?? 8000);
 }
 bootstrap();
